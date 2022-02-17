a52d56f457c746b978065cdc845ba2b50c2c44fc
santyanto <santy_antonelli@hotmail.com>
2022-02-17 16:43:19 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col">
+        <div className="col">
+        </div>
+        <div className="row">
+        <form className="col mb-5">
+          <h1 className="text">Inicie sesion</h1>
+          <div class="form-floating">
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"required/>
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating mb-5">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña"required/>
+          <label for="floatingPassword">Contraseña</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Iniciar Sesion</button>
+        </div>
+        </form>
+        <form className="col mb-5">
+          <h1 className="text">Registrarse</h1>
+          <div class="form-floating">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInput" placeholder="Nombre" required/>
+          <label for="floatingInput">Nombre</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInput" placeholder="Apellido"required/>
+          <label for="floatingInput">Apellido</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"required/>
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña"required/>
+          <label for="floatingPassword">Contraseña</label>
+        </div>
+        <div class="form-floating mb-5">
+          <input type="password" class="form-control" id="floatingInput" placeholder="Repita Contraseña"required/>
+          <label for="floatingInput">Repita Contraseña</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Registrarse</button>
+        </div>
+        </form>
+        </div>
+      </div>
     </div>
-  );
-}
+    </div>
+  )}
 
 export default App;
